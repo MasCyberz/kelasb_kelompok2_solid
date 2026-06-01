8df278a3ef20b5ebdcf818202e98177c1c702adf
ameliapinasti38-prog <ameliapinasti38@student.uns.ac.id>
2026-06-01 22:07:00 +0700

    Update kandang.py

--- habitat/kandang.py
+++ habitat/kandang.py
@@ -7,33 +7,29 @@
 # ==========================================
 
 class Kandang:
-    def __init__(self, nama_kandang: str, kapasitas: int):
+    def __init__(self, nama_kandang: str = "Kandang Utama"):
         self.nama_kandang = nama_kandang
         self.kapasitas = kapasitas
-        self.daftar_hewan = []  # Menampung list objek hewan
+        self.hewan_list = [] 
 
     def tambah_hewan(self, hewan) -> bool:
-
-        #[Memenuhi DIP & OCP] 
-        #Menerima objek 'hewan' secara umum lewat abstraksi.Kandang 
-        #tidak peduli jenis hewannya apa, selama ia adalah objek hewan.
-        if len(self.daftar_hewan) < self.kapasitas:
-            self.daftar_hewan.append(hewan)
+        # [Memenuhi DIP & OCP] 
+        # Dimana nanti program enerima objek 'hewan' secara umum lewat abstraksi.
+        if len(self.hewan_list) < self.kapasitas:
+            self.hewan_list.append(hewan)
             print(f"-> [INFO] {hewan.nama} berhasil dimasukkan ke {self.nama_kandang}.")
-            return True
+            return True 
         print(f"-> [PERINGATAN] {self.nama_kandang} sudah penuh! Gagal memasukkan {hewan.nama}.")
         return False
 
     def tampilkan_penghuni(self):
-    
-        #[Memenuhi SRP]
-        #Tugasnya hanya untuk mencetak/menampilkan daftar penghuni kandang.
+        # [Memenuhi SRP]
+        # Tugasnya khusus hanya untuk mencetak/menampilkan daftar penghuni kandang.
         print(f"\n===== DAFTAR PENGHUNI {self.nama_kandang.upper()} =====")
-        if not self.daftar_hewan:
+        if not self.hewan_list:
             print("Kandang ini masih kosong.")
             return
         
-        for index, hewan in enumerate(self.daftar_hewan, start=1):
-            # Mengambil nama class asli hewan (misal: Elang, Singa)
+        for index, hewan in enumerate(self.hewan_list, start=1):
             jenis_hewan = hewan.__class__.__name__
             print(f"{index}. {hewan.nama} ({jenis_hewan})")
