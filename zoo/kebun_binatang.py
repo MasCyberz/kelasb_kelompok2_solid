8073a44b4e567aa14e19d664ccd25a943317e42e
arindyawardah-create <arindyawardah@student.uns.ac.id>
2026-06-01 14:11:04 +0700

    update zoo
    ini baru lagi

--- zoo/kebun_binatang.py
+++ zoo/kebun_binatang.py
@@ -2,4 +2,12 @@ class KebunBinatang:
     def __init__(self, kandang):
         self.kandang = kandang
 
-    
+    def rawat_semua_hewan(self):
+        for hewan in self.kandang.hewan_list:
+            hewan.makan()
+
+            if isinstance(hewan, BisaTerbang):
+                hewan.terbang()
+            if isinstance(hewan, BisaBerenang):
+                hewan,berenang()
+            
